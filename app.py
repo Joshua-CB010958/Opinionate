15e47cd9395627eda1685bc161cc9f1682ca8031
Joshua <113645455+Joshua-CB010958@users.noreply.github.com>
2024-10-13 06:39:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,11 @@ from textblob import TextBlob
 import spacy
 from spacy.cli import download
 
-def download_model():
-    try:
-        spacy.cli.download("en_core_web_sm")
-    except Exception as e:
-        print(f"Error downloading model: {e}")
-
-def load_model():
-    try:
-        nlp = spacy.load("en_core_web_sm")
-        print("spaCy model loaded successfully.")
-    except OSError:
-        print("Model not found, downloading...")
-        download_model()
-        nlp = spacy.load("en_core_web_sm")
-        print("spaCy model downloaded and loaded successfully.")
-    return nlp
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 
 app = Flask(__name__)
